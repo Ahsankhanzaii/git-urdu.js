98b8edb3c601fb1800cb476ba738c4ef2b1eddf7
Ahsankhanzaii <ahsankhanzaii10@gmail.com>
2026-05-23 05:37:28 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all code as newer version
+
+//alert(3 + 3)// we are using nodejs ,not browser
+
+console.log(3 +
+     3) //code readibility should be high
+
+let name = "Ahsan"
+let age = 15 
+let isLoggedIn = true
+let state =      
+
+
+//number => 2 to power 53
+//Bigint 
+//string => ""
+//Bolean => True/False
+//Null => standalone value
+// undefined => 
+// symbol => unique
+
+
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
