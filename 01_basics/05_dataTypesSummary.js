dc5ba26cda0c95dad1db9edb6a5de223a831ab8d
Ahsankhanzaii <ahsankhanzaii10@gmail.com>
2026-06-06 07:25:06 +0000

    dataTypes Summary

---
+++ 01_basics/05_dataTypesSummary.js
@@ -0,0 +1,39 @@
+// Primitive 
+
+// 7 Types : Number, String, Bolean, Null, Undefined, Symbol, BigInt 
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmai;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 34567678597900n
+
+// console.log(typeof bigNumber);
+
+
+
+// Reference (Non Premitive)
+// Array, Objects, Functions
+
+const heros = ["chipaMan", "sparrow", "jawadWorld",];
+let myObj = {
+  name: "Ahsan",
+  age: 15
+
+}
+
+const myFunction = function() {
+    console.log("hello World");
+    
+}
+console.log(typeof heros);
+
